ca618d3d06d0c30cc176868784b7b4fc6ad3fe75
Johnathan Rafael Santos Borba <johnathan.developer@gmail.com>
2023-12-29 03:12:52 -0300

    :sparkles: feat: adding error return

--- languages.js
+++ languages.js
@@ -1,15 +1,19 @@
 const fetch = require('node-fetch');
 const fs = require('fs').promises;
 
+console.log('Iniciando script...');
+
 const username = 'dev-johnathan'; // Substitua pelo seu nome de usuário do GitHub
 
 async function fetchLanguages() {
   try {
+    console.log('Obtendo repositórios do GitHub API...');
     const response = await fetch(`https://api.github.com/users/${username}/repos`);
     const repos = await response.json();
 
     const languages = {};
 
+    console.log('Analisando repositórios...');
     repos.forEach(repo => {
       const repoLanguages = repo.language;
       if (repoLanguages) {
@@ -25,7 +29,9 @@ async function fetchLanguages() {
       .map(([language, count]) => `- ${language}: ${count}`)
       .join('\n');
 
+    console.log('Escrevendo em languages-output.md...');
     await fs.writeFile('languages-output.md', output);
+    console.log('Concluído.');
   } catch (error) {
     console.error('Erro ao recuperar informações do GitHub API:', error.message);
   }
